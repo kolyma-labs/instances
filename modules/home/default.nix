db0d3170e9cd96a8c7ddab43ccebb507f745417a
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-01 22:52:58 +0500

    added homeManagerModules to instances itself

---
+++ modules/home/default.nix
@@ -0,0 +1,13 @@
+# Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
+# These should be stuff you would like to share with others, not your personal configurations.
+{
+  # List your module files here
+  git = import ./git;
+  zsh = import ./zsh;
+  helix = import ./helix;
+  neovim = import ./neovim;
+  nixpkgs = import ./nixpkgs;
+  terminal = import ./terminal;
+  topgrade = import ./topgrade;
+  packages = import ./packages;
+}
