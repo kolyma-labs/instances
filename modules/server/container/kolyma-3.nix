11af408dcf874efa6afc5146593b07b3ad3a6dc4
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-02 22:17:23 +0500

    pulling telegram bot via sha256 tag

--- modules/server/container/kolyma-3.nix
+++ modules/server/container/kolyma-3.nix
@@ -26,7 +26,7 @@
     };
 
     bot-xinuxmgr = {
-      image = "ghcr.io/xinux-org/xinuxmgr:39ac164af8bf90e5900074adadc487c0a844bcab1b260853dcd3c8f57e73608a";
+      image = "ghcr.io/xinux-org/xinuxmgr@sha256:39ac164af8bf90e5900074adadc487c0a844bcab1b260853dcd3c8f57e73608a";
       ports = ["8445:8445"];
       environmentFiles = [
         /srv/bots/xinuxmgr.env
