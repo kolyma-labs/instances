13e968d02e77b43ddecdc6fb836e4a2633d34348
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-04 14:21:25 +0500

    remove minecraft server from kolyma-1

--- modules/server/container/kolyma-3.nix
+++ modules/server/container/kolyma-3.nix
@@ -54,28 +54,6 @@
     #     MEMORY = "12G";
     #   };
     # };
-
-    #    _______ __     ____
-    #   / ____(_) /_   / __ \__  ______  ____  ___  _____
-    #  / / __/ / __/  / /_/ / / / / __ \/ __ \/ _ \/ ___/
-    # / /_/ / / /_   / _, _/ /_/ / / / / / / /  __/ /
-    # \____/_/\__/  /_/ |_|\__,_/_/ /_/_/ /_/\___/_/
-    #   runner-1 = {
-    #     image = "gitlab/gitlab-runner:latest";
-    #     volumes = [
-    #       "/srv/git/runner-1:/etc/gitlab-runner"
-    #       "/var/run/docker.sock:/var/run/docker.sock"
-    #     ];
-    #   };
-
-    #   runner-2 = {
-    #     image = "gitlab/gitlab-runner:latest";
-    #     volumes = [
-    #       "/srv/git/runner-2:/etc/gitlab-runner"
-    #       "/var/run/docker.sock:/var/run/docker.sock"
-    #     ];
-    #   };
-    # };
   };
 
   # Necessary firewall rules for docker containers
