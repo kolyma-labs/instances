76cbc77c442bfab0f421c1b2fc83f7ad436e6b41
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-07-14 21:55:45 +0500

    Update kolyma-3.nix

--- modules/server/caddy/kolyma-3.nix
+++ modules/server/caddy/kolyma-3.nix
@@ -41,7 +41,12 @@
             "chat.haskell.uz"
           ];
           extraConfig = ''
-            reverse_proxy 127.0.0.1:8450
+            reverse_proxy 127.0.0.1:8450 {
+              header_up Host {host}
+          		header_up X-Real-IP {remote}
+          		header_up Upgrade {http_upgrade}
+          		header_up Connection {>Connection}
+            }
           '';
         };
 
