c7867fecbb35e9cd63f1c647eec0290afa3061d0
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-09-04 12:22:45 +0500

    adding sardor to wheel

--- modules/nixos/users/sardor.nix
+++ modules/nixos/users/sardor.nix
@@ -15,7 +15,7 @@
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATIYPqUdcP64tufjGii7LNncg5N2AzX7fj4JH3Vtdq2 mhw0@yahoo.com"
         ];
-        extraGroups = [ "docker" "admins" ];
+        extraGroups = [ "docker" "admins" "wheel" ];
       };
     };
 
