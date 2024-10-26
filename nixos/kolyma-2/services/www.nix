7497e97dd442d0fc8ac3fbc2fa390e8a20242423
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-10-27 03:34:18 +0500

    fix: remove localhost detection

--- nixos/kolyma-2/services/www.nix
+++ nixos/kolyma-2/services/www.nix
@@ -16,13 +16,7 @@
 
       "old.kolyma.uz" = {
         extraConfig = ''
-            reverse_proxy 127.0.0.1:8450 {
-              header_up Host {host}
-          		header_up X-Real-IP {remote}
-          		header_up Upgrade {http_upgrade}
-          		header_up Connection {>Connection}
-              header_up X-Forwarded-Proto {scheme}
-            }
+          reverse_proxy 127.0.0.1:8450
         '';
       };
     };
