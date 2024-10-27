5d72569bf8613f9fa4bda518200e2b78b41e11bc
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-10-27 05:07:23 +0500

    chore: extra configs for gitlab

--- nixos/kolyma-2/services/gitlab.nix
+++ nixos/kolyma-2/services/gitlab.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services.gitlab = {
     enable = true;
+    host = "git.kolyma.uz";
     databasePasswordFile = pkgs.writeText "dbPassword" "zgvcyfwsxzcwr85l";
     initialRootPasswordFile = pkgs.writeText "rootPassword" "dakqdvp4ovhksxer";
     secrets = {
@@ -9,6 +11,27 @@
       dbFile = pkgs.writeText "dbsecret" "HNWvNMIv9APPn9jl7K02Jh7EEpqtmPPrfgF7o0wUx4IrbmOFww";
       jwsFile = pkgs.runCommand "oidcKeyBase" { } "${pkgs.openssl}/bin/openssl genrsa 2048 > $out";
     };
+
+    smtp = {
+      enable = true;
+      port = 465;
+      tls = true;
+      opensslVerifyMode = "none";
+      address = "mail.kolyma.uz";
+      domain = "mail.kolyma.uz";
+      authentication = "plain";
+      enableStartTLSAuto = false;
+      username = "gitlab";
+      passwordFile = "/srv/git/mail-password.env";
+    };
+
+    extraGitlabRb = ''
+      #! If your SMTP server does not like the default 'From: gitlab@gitlab.example.com'
+      #! can change the 'From' with this setting.
+      gitlab_rails['gitlab_email_from'] = 'staff@kolyma.uz'
+      gitlab_rails['gitlab_email_display_name'] = 'Kolyma Git Station'
+      gitlab_rails['gitlab_email_reply_to'] = 'noreply@kolyma.uz'
+    '';
   };
 
   # Enable web server & proxy
