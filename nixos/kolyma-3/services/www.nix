93ade4ebc9bbded58459888c9292d6e5f7fe4ebe
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-11-24 21:59:02 +0500

    fix: (www) map subdomain for map observer

--- nixos/kolyma-3/services/www.nix
+++ nixos/kolyma-3/services/www.nix
@@ -18,10 +18,7 @@
         '';
       };
 
-      "slave.uz" = {
-        serverAliases = [
-          "www.slave.uz"
-        ];
+      "map.slave.uz" = {
         extraConfig = ''
           reverse_proxy 127.0.0.1:8100
         '';
