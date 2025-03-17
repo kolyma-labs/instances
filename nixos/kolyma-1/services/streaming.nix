699621eacee5732d3665a3b4ca5cd961689709e2
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-18 03:14:16 +0500

    chore: livestreaming let's goooo!

---
+++ nixos/kolyma-1/services/streaming.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+in {
+  config = {
+    services.owncast = {
+      enable = true;
+    };
+
+    services.www.hosts = {
+      "live.orzklv.uz" = {
+        extraConfig = ''
+          reverse_proxy ${config.services.owncast.listen}:${toString config.services.owncast.port}
+        '';
+      };
+    };
+
+    networking = {
+      firewall = {
+        enable = true;
+        allowedTCPPorts = [
+          config.services.owncast.rtmp-port
+        ];
+        allowedUDPPorts = [
+          config.services.owncast.rtmp-port
+        ];
+      };
+    };
+  };
+}
