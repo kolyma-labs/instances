276125118a846f76f7f99fee8e741126d6661e91
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-19 23:52:14 +0500

    chore: fetch key from github + single file

--- modules/nixos/users/sakhib.nix
+++ modules/nixos/users/sakhib.nix
@@ -6,23 +6,37 @@
 }: let
   username = "sakhib";
 
-  password =
-    builtins.replaceStrings
-    ["\n"] [""] (builtins.readFile ./password);
+  hashedPassword = lib.strings.concatStrings [
+    "$y$j9T$dsXOFHW"
+    "CyplfRPiwsKu0l"
+    "0$7YXPRLohyW8Q"
+    "XfyITPP6Sag/l7"
+    "XH3i7TO4uGByPK"
+    "Bb2"
+  ];
 in {
   config = {
     users.users = {
       "${username}" = {
+        inherit hashedPassword;
         isNormalUser = true;
         description = "Sokhibjon Orzikulov";
-        hashedPassword = password;
-        openssh.authorizedKeys.keys = [(builtins.readFile ./id_rsa.pub)];
+
         extraGroups = [
           "networkmanager"
           "wheel"
           "docker"
           "admins"
         ];
+
+        openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+          builtins.readFile (
+            builtins.fetchurl {
+              url = "https://github.com/orzklv.keys";
+              sha256 = "05rvkkk382jh84prwp4hafnr3bnawxpkb3w6pgqda2igia2a4865";
+            }
+          )
+        );
       };
     };
 
