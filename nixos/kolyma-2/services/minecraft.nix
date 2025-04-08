6dd94d20737c7617a30d90631e8e345535fd0e00
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-08 23:13:41 +0500

    chore: go with fabric nixified version

--- nixos/kolyma-2/services/minecraft.nix
+++ nixos/kolyma-2/services/minecraft.nix
@@ -1,34 +1,40 @@
 {
-  config,
+  inputs,
   lib,
+  pkgs,
   ...
 }: let
+  # Global variables
+  version = "1.19.2";
 in {
-  virtualisation.oci-containers.containers = {
-    minecraft = {
-      image = "itzg/minecraft-server:java8";
-      ports = [
-        "127.0.0.1:25565:25565"
-      ];
+  imports = [inputs.minecraft.nixosModules.minecraft-servers];
 
-      volumes = [
-        "/srv/minecraft:/data"
-      ];
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
 
-      environment = {
-        EULA = "TRUE";
-        TYPE = "FORGE";
-        MEMORY = "24G";
-        VERSION = "1.19.2";
-        FORGE_VERSION = "latest";
+    servers = {
+      slave = let
+        server = lib.replaceStrings ["."] ["_"] "fabric-${version}";
+      in {
+        enable = true;
+        openFirewall = true;
+        package = pkgs.fabricServers.${server}.override {loaderVersion = "0.14.6";};
+        jvmOpts = "-Xms12288M -Xmx12288M -XX:+UseG1GC -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
+
+        serverProperties = {
+          server-port = 25565;
+          difficulty = 3;
+          gamemode = 0;
+          max-players = 50;
+          online-mode = false;
+          motd = "\\u00A7f\\u00A7lWelcome to Sabine's Modded Server\\u00A7r\\n\\u00A7lMore at\:\\u00A7r \\u00A7nhttps\://mod.sabine.uz";
+          white-list = false;
+          enable-rcon = false;
+          "rcon.port" = 25575;
+          "rcon.password" = "F1st1ng15300Buck!?";
+        };
       };
-    };
-  };
-
-  networking = {
-    firewall = {
-      allowedUDPPorts = [25565];
-      allowedTCPPorts = [25565];
     };
   };
 }
