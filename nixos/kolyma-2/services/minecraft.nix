852e106a483e7e2a11b0ad8ff09d102752bac999
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-08 23:06:10 +0500

    fix: use clean forge

--- nixos/kolyma-2/services/minecraft.nix
+++ nixos/kolyma-2/services/minecraft.nix
@@ -17,25 +17,10 @@ in {
 
       environment = {
         EULA = "TRUE";
-        TYPE = "CRUCIBLE"; # CUSTOM
+        TYPE = "FORGE";
         MEMORY = "24G";
-
-        VERSION = "1.7.10";
-        CRUCIBLE_RELEASE = "latest";
-        # EXTRA_ARGS = "--log-strip-color";
-        # CUSTOM_SERVER = "https://github.com/CyberdyneCC/Thermos/releases/download/58/Thermos-1.7.10-1614-server.jar";
-        # JVM_OPTS = lib.strings.concatStringsSep " " [
-        #   "-XX:+UseG1GC"
-        #   "-XX:+OptimizeStringConcat"
-        #   "-XX:MetaspaceSize=2048m"
-        #   "-XX:MaxMetaspaceSize=4096m"
-        #   "-XX:ParallelGCThreads=15"
-        #   "-XX:MaxGCPauseMillis=10"
-        #   "-XX:+UseStringDeduplication"
-        #   "-XX:+UnlockExperimentalVMOptions"
-        #   "-XX:hashCode=5"
-        #   "-Dfile.encoding=UTF-8"
-        # ];
+        VERSION = "1.19.2";
+        FORGE_VERSION = "latest";
       };
     };
   };
