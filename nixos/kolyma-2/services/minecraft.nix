a5bc43aff3d057f15b4a83bb609819af4e326496
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-13 02:52:17 +0500

    chore: minecraft on docker attempt #2

---
+++ nixos/kolyma-2/services/minecraft.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  ...
+}: let
+in {
+  virtualisation.oci-containers.containers = {
+    minecraft = {
+      image = "itzg/minecraft-server:java17";
+      ports = [
+        "127.0.0.1:25565:25565"
+      ];
+
+      volumes = [
+        "/srv/minecraft:/data"
+      ];
+
+      environment = {
+        EULA = "TRUE";
+        TYPE = "FABRIC";
+        MEMORY = "24G";
+        FABRIC_LAUNCHER_VERSION = "1.0.3";
+        FABRIC_LOADER_VERSION = "0.16.13";
+        MODRINTH_PROJECTS = "fabric-api";
+      };
+    };
+  };
+
+  networking = {
+    firewall = {
+      allowedUDPPorts = [25565];
+      allowedTCPPorts = [25565];
+    };
+  };
+}
