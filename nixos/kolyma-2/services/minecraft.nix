d5fa6b42bbcbea692108ebb85f6266d6215a4ca0
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-13 06:57:25 +0500

    chore: added fear minecraft server

--- nixos/kolyma-2/services/minecraft.nix
+++ nixos/kolyma-2/services/minecraft.nix
@@ -5,7 +5,7 @@
 }: let
 in {
   virtualisation.oci-containers.containers = {
-    minecraft = {
+    minecraft-gamemania = {
       image = "itzg/minecraft-server:java21";
       ports = [
         "24454:24454"
@@ -13,7 +13,7 @@ in {
       ];
 
       volumes = [
-        "/srv/minecraft:/data"
+        "/srv/minecraft/gamemania:/data"
       ];
 
       environment = {
@@ -25,12 +25,33 @@ in {
         FABRIC_LOADER_VERSION = "0.16.13";
       };
     };
+
+    minecraft-fear = {
+      image = "itzg/minecraft-server:java21";
+      ports = [
+        "25566:25565"
+      ];
+
+      volumes = [
+        "/srv/minecraft/fear:/data"
+      ];
+
+      environment = {
+        EULA = "TRUE";
+        TYPE = "MODRINTH";
+        MEMORY = "24G";
+        VERSION = "1.20.1";
+        MODRINTH_MODPACK = "fearnightfall";
+        MODRINTH_VERSION = "kFoOyRmJ";
+        MODRINTH_LOADER = "forge";
+      };
+    };
   };
 
   networking = {
     firewall = {
-      allowedUDPPorts = [25565];
-      allowedTCPPorts = [25565];
+      allowedUDPPorts = [25565 25566];
+      allowedTCPPorts = [25565 25566];
     };
   };
 }
