7cf5dd94c67e60f1b2948f1661f84e765ace8a32
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-01 22:02:57 +0500

    chore: attempting with custom server

--- nixos/kolyma-2/services/minecraft.nix
+++ nixos/kolyma-2/services/minecraft.nix
@@ -15,47 +15,10 @@ in {
 
       environment = {
         EULA = "TRUE";
-        TYPE = "FORGE";
+        TYPE = "CUSTOM";
+        CUSTOM_SERVER = "https://adfoc.us/serve/sitelinks/?id=765928&url=https://mohistmc.com/api/v2/projects/mohist/1.19.2/builds/873d239111d0a374c2a9a5d6bfbcffe2087d94f8/mohist-1.19.2-873d2391-server.jar";
         MEMORY = "24G";
         VERSION = "1.19.2";
-        FORGE_VERSION = "latest";
-      };
-    };
-
-    minecraft-fear = {
-      image = "itzg/minecraft-server:java17";
-      ports = [
-        "25566:25565"
-      ];
-
-      volumes = [
-        "/srv/minecraft/fear:/data"
-      ];
-
-      environment = {
-        EULA = "TRUE";
-        TYPE = "FORGE";
-        MEMORY = "24G";
-        VERSION = "1.20.1";
-        FORGE_VERSION = "latest";
-      };
-    };
-
-    minecraft-deceased = {
-      image = "itzg/minecraft-server:java17";
-      ports = [
-        "25567:25565"
-      ];
-
-      volumes = [
-        "/srv/minecraft/deceased:/data"
-      ];
-
-      environment = {
-        EULA = "TRUE";
-        TYPE = "FORGE";
-        MEMORY = "24G";
-        VERSION = "1.18.2";
         FORGE_VERSION = "latest";
       };
     };
@@ -63,8 +26,8 @@ in {
 
   networking = {
     firewall = {
-      allowedUDPPorts = [25565 25566 25567 25575];
-      allowedTCPPorts = [25565 25566 25567 25575];
+      allowedUDPPorts = [24454 25565 25575];
+      allowedTCPPorts = [24454 25565 25575];
     };
   };
 }
