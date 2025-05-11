debb3cf198a99dff7b0c06b9310dd2686b8591a6
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-11 10:35:34 +0500

    chore: ns5 clean-up

--- nixos/kolyma-5/services/www.nix
+++ nixos/kolyma-5/services/www.nix
@@ -4,22 +4,8 @@
   # Enable web server & proxy
   services.www = {
     enable = true;
-    domain = "slave.uz";
-    alias = [
-      "ns3.kolyma.uz"
-    ];
-    hosts = {
-      "sabine.uz" = {
-        addSSL = true;
-        enableACME = true;
-        serverAliases = ["www.sabine.uz"];
-
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8100";
-          proxyWebsockets = true;
-          extraConfig = "proxy_ssl_server_name on;";
-        };
-      };
-    };
+    alias = ["uz1.kolyma.uz"];
+    no-default = true;
+    hosts = {};
   };
 }
