dc20ae3f83dbbd3724f434b879b7f03230c79587
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-13 20:58:32 +0500

    chore: adding coturn service

--- nixos/kolyma-2/services/matrix.nix
+++ nixos/kolyma-2/services/matrix.nix
@@ -4,8 +4,15 @@
   config,
   ...
 }: let
+  # Matrix configs
   domain = "floss.uz";
   server = "matrix.${domain}";
+
+  # Coturn configs
+  realm = "turn.floss.uz";
+  static-auth-secret = "the most niggerlicious thing is to use javascript and python :(";
+
+  # Matrix Client Application
   client = {
     address = "chat.${domain}";
     pkg = pkgs.element-web.override {
@@ -157,6 +164,10 @@ in {
         server_name = domain;
         public_baseurl = "https://${server}";
 
+        turn_uris = ["turn:${realm}:3478?transport=udp" "turn:${realm}:3478?transport=tcp"];
+        turn_shared_secret = static-auth-secret;
+        turn_user_lifetime = "1h";
+
         allow_guest_access = true;
         enable_registration = true;
         registrations_require_3pid = ["email"];
@@ -203,8 +214,74 @@ in {
       };
     };
 
+    services.coturn = rec {
+      inherit realm static-auth-secret;
+      enable = true;
+      no-cli = true;
+      no-tcp-relay = true;
+      min-port = 49000;
+      max-port = 50000;
+      use-auth-secret = true;
+      cert = "${config.security.acme.certs.${realm}.directory}/full.pem";
+      pkey = "${config.security.acme.certs.${realm}.directory}/key.pem";
+      extraConfig = ''
+        # for debugging
+        verbose
+        # ban private IP ranges
+        no-multicast-peers
+        denied-peer-ip=0.0.0.0-0.255.255.255
+        denied-peer-ip=10.0.0.0-10.255.255.255
+        denied-peer-ip=100.64.0.0-100.127.255.255
+        denied-peer-ip=127.0.0.0-127.255.255.255
+        denied-peer-ip=169.254.0.0-169.254.255.255
+        denied-peer-ip=172.16.0.0-172.31.255.255
+        denied-peer-ip=192.0.0.0-192.0.0.255
+        denied-peer-ip=192.0.2.0-192.0.2.255
+        denied-peer-ip=192.88.99.0-192.88.99.255
+        denied-peer-ip=192.168.0.0-192.168.255.255
+        denied-peer-ip=198.18.0.0-198.19.255.255
+        denied-peer-ip=198.51.100.0-198.51.100.255
+        denied-peer-ip=203.0.113.0-203.0.113.255
+        denied-peer-ip=240.0.0.0-255.255.255.255
+        denied-peer-ip=::1
+        denied-peer-ip=64:ff9b::-64:ff9b::ffff:ffff
+        denied-peer-ip=::ffff:0.0.0.0-::ffff:255.255.255.255
+        denied-peer-ip=100::-100::ffff:ffff:ffff:ffff
+        denied-peer-ip=2001::-2001:1ff:ffff:ffff:ffff:ffff:ffff:ffff
+        denied-peer-ip=2002::-2002:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+        denied-peer-ip=fc00::-fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+        denied-peer-ip=fe80::-febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+      '';
+    };
+
+    networking.firewall = let
+      range = with config.services.coturn; [
+        {
+          from = min-port;
+          to = max-port;
+        }
+      ];
+    in {
+      allowedUDPPortRanges = range;
+      allowedUDPPorts = [3478 5349];
+      allowedTCPPortRanges = [];
+      allowedTCPPorts = [3478 5349];
+    };
+
+    users.users.nginx.extraGroups = [config.users.groups.turnserver.name];
+
+    security.acme.certs.${config.services.coturn.realm} = {
+      postRun = "systemctl restart coturn.service";
+      group = lib.mkForce "turnserver";
+    };
+
     services.www.hosts = {
-      "${server}" = {
+      ${realm} = {
+        addSSL = true;
+        enableACME = true;
+      };
+
+      ${server} = {
         addSSL = true;
         enableACME = true;
 
