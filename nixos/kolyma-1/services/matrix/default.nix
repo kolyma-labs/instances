f5f194f7e1f43099847e1b4737d2566d4fe99f16
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-06-08 12:56:58 +0500

    chore: passing necessary args to lambda

--- nixos/kolyma-1/services/matrix/default.nix
+++ nixos/kolyma-1/services/matrix/default.nix
@@ -27,9 +27,9 @@ in {
     outputs.nixosModules.mas
 
     # Parts of this configuration
-    (import ./proxy {inherit lib domains pkgs;})
     (import ./call.nix {inherit config domains;})
     (import ./auth.nix {inherit config domains;})
+    (import ./proxy {inherit lib domains pkgs config;})
     (import ./mail.nix {inherit inputs domains config;})
     (import ./turn.nix {inherit lib config domains keys;})
     (import ./server.nix {inherit lib config domains keys;})
