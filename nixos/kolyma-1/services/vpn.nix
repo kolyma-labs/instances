ed4347dff370eb31776f1ba6d359cd78f7d38e82
orzklv <sakhib@orzklv.uz>
2025-08-01 18:56:00 +0500

    fix: comment out unavailable amd64 instance tests

---
+++ nixos/kolyma-1/services/vpn.nix
@@ -0,0 +1,51 @@
+{
+  outputs,
+  pkgs,
+  ...
+}: {
+  imports = [outputs.nixosModules.nginx];
+
+  # Enable web server & proxy
+  services.www = {
+    enable = true;
+    alias = ["ns1.kolyma.uz"];
+    hosts = {
+      "cdn1.kolyma.uz" = {
+        addSSL = true;
+        enableACME = true;
+        root = "/srv/cdn";
+        extraConfig = ''
+          autoindex on;
+        '';
+      };
+
+      "khakimovs.uz" = {
+        addSSL = true;
+        enableACME = true;
+        serverAliases = ["www.khakimovs.uz"];
+        root = "${pkgs.personal.khakimovs}/www";
+
+        locations."/".extraConfig = ''
+          try_files $uri $uri/ $uri.html =404;
+        '';
+
+        locations."~ ^/(.*)\\.html$".extraConfig = ''
+          rewrite ^/(.*)\\.html$ /$1 permanent;
+        '';
+      };
+
+      "niggerlicious.uz" = {
+        addSSL = true;
+        enableACME = true;
+        serverAliases = ["www.niggerlicious.uz"];
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8100";
+          proxyWebsockets = true;
+          extraConfig =
+            "proxy_ssl_server_name on;"
+            + "proxy_pass_header Authorization;";
+        };
+      };
+    };
+  };
+}
