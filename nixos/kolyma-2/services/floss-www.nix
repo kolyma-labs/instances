4f0da77c4a405f6908371c03ed1aef12de861dec
orzklv <sakhib@orzklv.uz>
2025-09-22 02:37:01 +0500

    fix: remove unnecessary configs

--- nixos/kolyma-2/services/floss-www.nix
+++ nixos/kolyma-2/services/floss-www.nix
@@ -42,13 +42,6 @@ in {
 
       locations."/" = {
         proxyPass = "http://unix:${config.services.anubis.instances.floss.settings.BIND}";
-        extraConfig = ''
-          proxy_http_version 1.1;
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
       };
     };
   };
