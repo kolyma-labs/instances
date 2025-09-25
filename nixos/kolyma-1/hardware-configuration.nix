86451ee5bafc6d19e5433c5920febadc86a31808
orzklv <sakhib@orzklv.uz>
2025-09-26 00:19:01 +0500

    chore: add 1-2

---
+++ nixos/kolyma-1/hardware-configuration.nix
@@ -0,0 +1,59 @@
+{
+  inputs,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    # Disko partitioning
+    inputs.disko.nixosModules.disko
+    ./disk-configuration.nix
+
+    # Not available hardware modules
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    kernelModules = [];
+    extraModulePackages = [];
+
+    initrd = {
+      kernelModules = [
+        "nvme"
+        "kvm-intel"
+      ];
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "nvme"
+        "usbhid"
+      ];
+    };
+
+    bios = {
+      enable = true;
+      uefi = true;
+      raided = true;
+      mirrors = [
+        "/dev/nvme0n1"
+        "/dev/nvme1n1"
+      ];
+    };
+  };
+
+  network = {
+    enable = true;
+
+    ipv4 = {
+      enable = true;
+      address = "37.27.67.190";
+    };
+
+    ipv6 = {
+      enable = true;
+      address = "2a01:4f9:3081:3518::2";
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
