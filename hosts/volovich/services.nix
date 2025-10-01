5a334b1894a3269cd2016491a906bf46d96a7abb
orzklv <sakhib@orzklv.uz>
2025-10-01 11:06:37 +0500

    chore: even more runners

--- hosts/volovich/services.nix
+++ hosts/volovich/services.nix
@@ -37,15 +37,51 @@ in {
       enable = true;
       instances = [
         {
-          name = "Xinux";
-          url = "https://github.com/xinux-org";
-          token = config.sops.secrets."github/xinux".path;
+          name = "Default";
+          url = "https://github.com/kolyma-labs";
+          token = config.sops.secrets."github/kolyma".path;
           type = "github";
         }
         {
           name = "Uzinfocom";
           url = "https://github.com/uzinfocom-org";
           token = config.sops.secrets."github/uzinfocom".path;
+          type = "github";
+        }
+        {
+          name = "Floss";
+          url = "https://github.com/floss-uz";
+          token = config.sops.secrets."github/floss".path;
+          type = "github";
+        }
+        {
+          name = "Floss-Community";
+          url = "https://github.com/floss-uz-community";
+          token = config.sops.secrets."github/floss-community".path;
+          type = "github";
+        }
+        {
+          name = "Xinux";
+          url = "https://github.com/xinux-org";
+          token = config.sops.secrets."github/xinux".path;
+          type = "github";
+        }
+        {
+          name = "Rust";
+          url = "https://github.com/rust-lang-uz";
+          token = config.sops.secrets."github/rust-lang".path;
+          type = "github";
+        }
+        {
+          name = "Efael";
+          url = "https://github.com/efael";
+          token = config.sops.secrets."github/efael".path;
+          type = "github";
+        }
+        {
+          name = "Bleur";
+          url = "https://github.com/bleur-org";
+          token = config.sops.secrets."github/bleur".path;
           type = "github";
         }
       ];
