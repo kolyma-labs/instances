915757587a5836390eed28b34a7c334c07612faf
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-10-11 15:12:31 +0500

    chore: adding git server host to bind

--- hosts/solzhenitsyn/services.nix
+++ hosts/solzhenitsyn/services.nix
@@ -46,7 +46,7 @@
     };
 
     git = {
-      enable = true;
+      enable = false;
       domain = "git.floss.uz";
       mail = config.sops.secrets."git/mail".path;
       database = config.sops.secrets."git/database".path;
