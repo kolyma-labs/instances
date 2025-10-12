f1b59efa4e3457a24695023449718c41d303249b
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-10-13 02:16:44 +0500

    chore: add other platforms support

--- overlays/pkgs/element-stickerpicker/default.nix
+++ overlays/pkgs/element-stickerpicker/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   stdenvNoCC,
   element-stickers,
   maunium-stickerpicker,
@@ -6,9 +7,12 @@
 stdenvNoCC.mkDerivation {
   src = maunium-stickerpicker;
   name = "element-stickers";
+
   buildPhase = ''
     mv web $out/
     cp ${element-stickers}/uploaded-packs/*.json $out/packs/
     cp -r ${element-stickers}/uploaded-packs/thumbnails $out/packs/
   '';
+
+  meta.platforms = lib.platforms.all;
 }
--- overlays/pkgs/element-themes/default.nix
+++ overlays/pkgs/element-themes/default.nix
@@ -1,13 +1,17 @@
 {
+  lib,
   stdenvNoCC,
   jq,
   element-themes,
 }:
 stdenvNoCC.mkDerivation {
-  src = element-themes;
   name = "element-themes";
+  src = element-themes;
   nativeBuildInputs = [jq];
+
   buildPhase = ''
     find "$src" -name '*.json' -print0 | xargs -0 jq -s '.' > $out
   '';
+
+  meta.platforms = lib.platforms.all;
 }
