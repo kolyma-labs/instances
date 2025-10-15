f8c4525002e5c88631f36eda11f36e2c80a319f8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-10-15 15:43:57 +0500

    chore: it's working but why?

--- modules/mastodon/proxy.nix
+++ modules/mastodon/proxy.nix
@@ -122,8 +122,7 @@ in {
               add_header X-Content-Type-Options 'nosniff';
               add_header Content-Security-Policy "default-src 'none'; form-action 'none'";
             '';
-            # tryFiles = "$uri =404";
-            return = "200 'goofy ahh nigger'";
+            tryFiles = "$uri =404";
           };
 
           locations."^~ /api/v1/streaming" = {
