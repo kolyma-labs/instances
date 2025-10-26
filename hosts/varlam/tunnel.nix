a393d3b1f59fe74c331c03260e0a91f4b0ec2911
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-10-27 02:41:53 +0500

    fix: keepalive + mtu

--- hosts/varlam/tunnel.nix
+++ hosts/varlam/tunnel.nix
@@ -10,18 +10,16 @@
 
   networking.wireguard.interfaces = {
     wg-ssh = {
+      mtu = 1300;
       listenPort = 51820;
       ips = ["10.100.0.11/32"];
       privateKeyFile = config.sops.secrets."vpn/varlam".path;
       peers = [
         {
+          persistentKeepalive = 25;
           endpoint = "213.230.122.228:666";
           publicKey = "oDSqZC1ki2LtCF3pU7Zp2QhrXiHC4pV6o2ljaufyyxA=";
-          allowedIPs = [
-            "10.10.1.0/24"
-            "10.100.0.0/24"
-          ];
-          persistentKeepalive = 25;
+          allowedIPs = ["10.10.1.0/24" "10.100.0.0/24"];
         }
       ];
     };
