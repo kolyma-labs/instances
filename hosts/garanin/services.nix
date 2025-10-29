84e58ade9e2af8d6f008e573acdaa6ee8d70b488
orzklv <sakhib@orzklv.uz>
2025-10-30 00:43:04 +0500

    chore: uz1 as ns5

--- hosts/garanin/services.nix
+++ hosts/garanin/services.nix
@@ -4,36 +4,24 @@
     outputs.nixosModules.web
 
     # Per app preconfigured abstractions
-    outputs.nixosModules.apps.khakimovs-website
+    # outputs.nixosModules.apps.khakimovs-website
   ];
 
   # Kolyma services
   kolyma = {
-    # https://ns4.kolyma.uz
+    # https://ns5.kolyma.uz
     www = {
       enable = true;
-      instance = 4;
+      instance = 5;
     };
-
-    # bind://ns4.kolyma.uz
-    nameserver = {
-      enable = true;
-      type = "slave";
-    };
-
-    # https://cloud.floss.uz
-    nextcloud.enable = true;
-
-    # mc://niggerlicious.uz
-    minecraft.enable = true;
 
     # *://*
-    apps = {
-      # https://*.khakimovs.uz
-      khakimovs = {
-        # https://khakimovs.uz
-        website.enable = true;
-      };
-    };
+    # apps = {
+    #   # https://*.khakimovs.uz
+    #   khakimovs = {
+    #     # https://khakimovs.uz
+    #     website.enable = true;
+    #   };
+    # };
   };
 }
