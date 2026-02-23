80493918794f462c254919a1f2f49eb27423f6f5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-24 02:58:24 +0500

    chore: remove galitskiy and garanin

--- hosts/galitsky/easter-egg.nix
+++
@@ -1,7 +0,0 @@
-{
-  name = "Pavel Galitsky";
-  more = "https://www.opendemocracy.net/en/odr/last-prisoner/";
-  instance = -2;
-  location = "Uzbekistan";
-  description = "Last Jail of Kolyma";
-}
--- hosts/garanin/easter-egg.nix
+++
@@ -1,7 +0,0 @@
-{
-  name = "Stepan Garanin";
-  more = "https://biographs.org/stepan-garanin";
-  instance = -1;
-  location = "Uzbekistan";
-  description = "Chief of Transmitter";
-}
